85c43b05a8495c8e1707b82614fa23f900b3bd06
António Barros e Cunha <antoniobcunha@tecnico.ulisboa.pt>
2023-06-05 17:52:46 +0100

    cilindro tem bug

--- src/ufo.js
+++ src/ufo.js
@@ -5,20 +5,22 @@ class Ufo extends THREE.Object3D {
 	#cabin
 	#ship
 	#spotlight
+	#spotlightHousing
 
 	constructor() {
 		super();
 		this.cabin = this.#generateCabin();
 		this.ship = this.#generateShip();
 		this.spotlight = this.#generateSpotlight();
-
+		this.spotlightHousing = this.#generateSpotlightHousing();
+		this.spotlightHousing.position.set(0, -10, 0);
 		this.cabin.position.set(0,30,0);
 		this.spotlight.position.set(0,-10,0);
-		this.add(this.cabin, this.ship, this.spotlight);
+		this.add(this.cabin, this.ship, this.spotlight, this.spotlightHousing);
 	}
 
 	#generateCabin() {
-		const sphereGeometry = new THREE.SphereGeometry(80, 32, 32, 0, 2 * Math.PI, 0, (3 / 4) * Math.PI);
+		const sphereGeometry = new THREE.SphereGeometry(40, 32, 32, 0, 2 * Math.PI, 0, (3 / 4) * Math.PI);
 		const sphereMesh = new THREE.Mesh(
 			sphereGeometry,
 			new THREE.MeshBasicMaterial({
@@ -31,7 +33,7 @@ class Ufo extends THREE.Object3D {
 	#generateShip() {
 		const ellipsoidGeometry = new THREE.SphereGeometry(20);
 
-		ellipsoidGeometry.scale(10, 0.5, 10);
+		ellipsoidGeometry.scale(5, 1.5, 5);
 		const ellipsoidMesh = new THREE.Mesh(ellipsoidGeometry,
 			new THREE.MeshBasicMaterial({
 					color: 0x444444,
@@ -41,8 +43,17 @@ class Ufo extends THREE.Object3D {
 		return ellipsoidMesh;
 	}
 
+	#generateSpotlightHousing() {
+		const tubeGeom = new THREE.CylinderGeometry({radiusTop: 40, radiusBottom: 40, height : 30, openEnded : true});
+		const tubeMat  = new THREE.MeshBasicMaterial( {
+			color: 0x444444
+		});
+		const tubeMesh = new THREE.Mesh( tubeGeom, tubeMat);
+		return tubeMesh;		
+	}
+
 	#generateSpotlight() {
-		let spotlight = new THREE.SpotLight('lightyellow', 5, 20);
+		const spotlight = new THREE.SpotLight('lightyellow', 5, 20);
 		spotlight.castShadow = true;
 
 		spotlight.shadow.mapSize.width = 1024;
@@ -51,6 +62,10 @@ class Ufo extends THREE.Object3D {
 		spotlight.shadow.camera.near = 30;
 		spotlight.shadow.camera.far = 500;
 		spotlight.shadow.camera.fov = 30;
+		spotlight.translateY(-30);
+		const target = new THREE.Object3D();
+		target.translateY(-300);
+		spotlight.target = target;
 		return spotlight;
 	}
 
