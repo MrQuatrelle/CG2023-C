dd5a1fac9a983d18255b37deb42056cfe36c89c7
António Barros e Cunha <antoniobcunha@tecnico.ulisboa.pt>
2023-06-08 20:01:36 +0100

    lights good, helpers good, materials toon wtf, rotation good, move wtf

--- src/main.js
+++ src/main.js
@@ -10,7 +10,6 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 var ufoShip, camera;
 
-
 main();
 
 function main() {
@@ -21,39 +20,68 @@ function main() {
 	const axesHelper = new THREE.AxesHelper(1000);
 	cameraControl.setTarget(new THREE.Vector3(95, 240, 110));
 	camera = cameraControl.camera1;
-
-	ufoShip = new ufo.Ufo();
-	ufoShip.castShadow =true;
-	ufoShip.receiveShadow = true;
-	//tempFloor();
-	scene.add(ufoShip,axesHelper);
+	dropShip();
+	scene.add(axesHelper);
 
 
 	animate();
 }
+
 function tempFloor(){
 	const floor = new THREE.Mesh(new THREE.PlaneGeometry( 500, 500, 1, 1), new THREE.MeshPhongMaterial({color: 0x111180, side: THREE.DoubleSide}));
 	floor.rotateX(Math.PI/2);
 	floor.position.set(0,0,0);
 	scene.add(floor);
-	//scene.add(new THREE.AmbientLight("lightyellow", 0.02))
+	scene.add(new THREE.AmbientLight("lightyellow", 0.02))
 }
+
 function animate() {
 	// tldr, everytime the program has time to render a frame, it'll call this
 	// function
 
 	window.addEventListener("keydown", keydownHandler);
-	ufoShip.rotateY(Math.PI/128);
+	ufoShip.rotate();
+
 	renderer.render(scene, camera);
 	requestAnimationFrame(animate);
+}
+
+let keysPressed = {};
+
+function dropShip(){
+	ufoShip = new ufo.Ufo();
+	ufoShip.translateY(200);
+	tempFloor();
+	scene.add(ufoShip);
+	addHelpers();
+}
+
+function removeHelpers(){
+	let count = 0;
+	scene.children.forEach((c) =>{
+		if(c.type === "SpotLightHelper" ||
+			c.type === "PointLightHelper"){
+			console.log(c.type, "removed!");
+			scene.remove(c);
+			count++;
+		}
+	})
+	if(count ==0)
+		console.log("There are no Helpers around these areas.")
+}
+
+function addHelpers(){
+	ufoShip.getPointLightHelpers().forEach((c) =>{
+		 scene.add(c);
+	});
+	scene.add(ufoShip.getSpotLightHelper());
+
 }
 
 window.addEventListener("resize", () => {
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	cameraControl.update();
 });
-
-let keysPressed = {};
 
 function keydownHandler(e) {
 	keysPressed[e.key] = true;
@@ -88,6 +116,18 @@ function keydownHandler(e) {
 			console.log("[INFO]: showing camera6");
 			camera = cameraControl.camera6;
 			break;
+		case 'q':
+			console.log("[INFO]: Setting Materials as Lambert.")
+			ufoShip.changeMaterials(0);
+			break;
+		case 'w':
+			console.log("[INFO]: Setting Materials as Phong.")
+			ufoShip.changeMaterials(1);
+			break;
+		case 'e':
+			console.log("[INFO]: Setting Materials as Toon.")
+			ufoShip.changeMaterials(2);
+			break;
 		case 'p':
 			ufoShip.togglePointLights();
 			console.log("[INFO]:\tToggling UFO Point Lights...\n\tCurrent state: ", ufoShip.getPointLights().getLightsState(), e.key);
@@ -95,6 +135,33 @@ function keydownHandler(e) {
 		case 's':
 			ufoShip.toggleSpotlight();
 			console.log("[INFO]:\tToggling UFO Spotlight...\n\tCurrent state: ", ufoShip.getSpotlightState());
+			break;
+		case 'x':
+			removeHelpers();
+			break;
+
+		case 'ArrowLeft':
+			var clock = new THREE.Clock();
+			delta = clock.getDelta();
+			ufoShip.moveLeft(clock, delta);
+			break;
+
+		case 'ArrowRight':
+			var clock = new THREE.Clock();
+			delta = clock.getDelta();
+			ufoShip.moveRight(clock, delta);
+			break;
+
+		case 'ArrowDown':
+			var clock = new THREE.Clock();
+			delta = clock.getDelta();
+			ufoShip.moveBackwards(clock, delta);
+			break;
+
+		case 'ArrowUp':
+			var clock = new THREE.Clock(clock);
+			delta = clock.getDelta();
+			ufoShip.moveForwards(clock, delta);
 			break;
 
 		default:
--- src/ufo_lights.js
+++ src/ufo_lights.js
@@ -2,44 +2,75 @@ import * as THREE from "three"
 class UFO_Lights extends THREE.Object3D {
 	
 	#lights;
-	constructor() {
+	#lightHelpers;
+	#bulbMaterials;
+	constructor(array =[]) {
 		super();
-		this.lights = this.#generateLights();
-		this.lights.castShadow = true;
-		this.add(this.lights);
+		this.#bulbMaterials = [
+			new THREE.MeshLambertMaterial({
+					color: 0x0000FF,
+					transparent: true
+				}),
+			new THREE.MeshPhongMaterial({
+					color: 0x0000FF,
+					transparent: true
+				}),
+			new THREE.MeshToonMaterial({
+					color: 0x0000FF,
+					transparent: true
+				})];
+		this.#lightHelpers = array;
+		this.#lights = this.#generateLights();
+		this.add(this.#lights);
 	}
 
 	#generateLights(){
 		const radius = 80;
-		const lights = new THREE.Object3D();
+		const lights= new THREE.Object3D();
 		for(let i = 0; i<8; i++){
 			let bulbsGeom = new THREE.SphereGeometry( 5, 32, 16, 0, 2*Math.PI, 0, Math.PI );
-			let bulbs = new THREE.Mesh( bulbsGeom,
-				new THREE.MeshPhongMaterial(
-				{
-					color: 0x0000FF,
-					transparent: true
-				}));
-			let light = new THREE.PointLight('white', 10, 300, 2);
-			let light_helper = new THREE.PointLightHelper(light, 3);
-			light.add(light_helper);
+			let bulbs = new THREE.Mesh( bulbsGeom, this.#bulbMaterials[0]);
+			let light = new THREE.PointLight('white', 10, 30, 2);
 			bulbs.position.set(radius*Math.cos(i*Math.PI/4),0,radius*Math.sin(i*Math.PI/4));
-			light.position.set((radius+10)*Math.cos(i*Math.PI/4),-30,(radius+10)*Math.sin(i*Math.PI/4));
+			light.position.set((radius)*Math.cos(i*Math.PI/4),-5,(radius)*Math.sin(i*Math.PI/4));
 			light.castShadow= true;
-			light.shadow.camera.near = 1;
+			light.shadow.camera.near = 0.1;
 			light.shadow.camera.far = 500;
+			light.shadow.mapSize.width = 512;
+			light.shadow.mapSize.height = 512;
+			this.#lightHelpers[i] = new THREE.PointLightHelper(light,5);
 			lights.add(bulbs, light);
 		}
 		return lights;
 	}
 	toggleLights (){
-		this.lights.visible = !this.lights.visible;
+		this.#lights.visible = !this.#lights.visible;
 	}
 	getLights(){
-		return this.lights;
+		return this.#lights;
+	}
+	getLightHelpers(){
+		return this.#lightHelpers;
 	}
 	getLightsState(){
-		return this.lights.visible;
+		return this.#lights.visible;
+	}
+	changeMaterials(type){
+		let count = 0;
+		this.#lights.children.forEach((c) =>{
+			if(c.isMesh){
+				if(c.material != this.#bulbMaterials[type]){
+					c.material = this.#bulbMaterials[type];
+					count++;
+				}
+				else{
+					console.log("[INFO]: This Material was already enabled.")
+				}
+			}
+		})
+		if(count = 0){
+			console.log("[INFO]: No materials changed")
+		}
 	}
 }
 
