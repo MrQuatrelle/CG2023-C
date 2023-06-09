f84bf7fc2924b952283b0f89dc757374da0b74db
António Barros e Cunha <antoniopcbc@gmail.com>
2023-06-09 20:19:46 +0100

    working as intended kinda

--- src/fullProject.js
+++ src/fullProject.js
@@ -290,66 +290,6 @@ class Moon extends THREE.Object3D{
 
 //========================================================================
 
-const heightMapTexture = new THREE.TextureLoader().load(
-    "../heightmap.png",
-    undefined,
-    undefined,
-    () => {
-        console.error("fail to source heightmap");
-    }
-);
-
-
-const terrainMaterials = [
-    new THREE.MeshLambertMaterial({
-        color: 0x92745b,
-        map: floorTexture,
-        bumpMap: heightMapTexture,
-        displacementMap: heightMapTexture,
-        displacementScale: 3000,
-        shadowSide: THREE.DoubleSide,
-    }),
-    new THREE.MeshPhongMaterial({
-        color: 0x92745b,
-        map: floorTexture,
-        bumpMap: heightMapTexture,
-        displacementMap: heightMapTexture,
-        displacementScale: 3000,
-        shininess: 5,
-        shadowSide: THREE.DoubleSide,
-    }),
-    new THREE.MeshToonMaterial({
-        color: 0x92745b,
-        map: floorTexture,
-        bumpMap: heightMapTexture,
-        displacementMap: heightMapTexture,
-        displacementScale: 3000,
-        shadowSide: THREE.DoubleSide,
-    }),
-    new THREE.MeshBasicMaterial({
-        color: 0x92745b,
-        map: floorTexture,
-        shadowSide: THREE.DoubleSide,
-    })
-];
-
-class Terrain extends THREE.Object3D{
-    #terrain
-    constructor(){
-        super();
-        const geometry = new THREE.PlaneGeometry(5000, 5000, 500, 20);
-        this.#terrain = new THREE.Mesh(geometry, terrainMaterials[0]);
-        this.#terrain.rotateX(Math.PI / -2);
-        this.#terrain.geometry.computeVertexNormals();
-        this.add(this.#terrain)
-
-    }
-    changeMaterials(type){
-        this.#terrain.material = terrainMaterials[type];
-    }
-
-}
-
 
 //===========================================================================
 
@@ -439,13 +379,74 @@ floorTexture.wrapT = THREE.RepeatWrapping;
 floorTexture.repeat.set(1, 1); 
 
 //cria o plano e adiciona textura
-var floorMaterial = new THREE.MeshBasicMaterial({
-  map: floorTexture, 
-  side: THREE.FrontSide //frontside significa que só dá load à parte de dentro da esfera 
-});
+//var floorMaterial = new THREE.MeshBasicMaterial({
+//  map: floorTexture, 
+//  side: THREE.FrontSide //frontside significa que só dá load à parte de dentro da esfera 
+//});
 //var floorGeometry = new THREE.PlaneGeometry(600, 600);
 //var floorText = new THREE.Mesh(floorGeometry, floorMaterial);
 //floorText.rotation.x = -Math.PI / 2; // Rotate 90 degrees around the x-axis
+
+//====================================================================
+const heightMapTexture = new THREE.TextureLoader().load(
+    "../heightmap.png",
+    undefined,
+    undefined,
+    () => {
+        console.error("fail to source heightmap");
+    }
+);
+
+
+const terrainMaterials = [
+    new THREE.MeshLambertMaterial({
+        color: 0x92745b,
+        map: floorTexture,
+        bumpMap: heightMapTexture,
+        displacementMap: heightMapTexture,
+        displacementScale: 3000,
+        shadowSide: THREE.DoubleSide,
+    }),
+    new THREE.MeshPhongMaterial({
+        color: 0x92745b,
+        map: floorTexture,
+        bumpMap: heightMapTexture,
+        displacementMap: heightMapTexture,
+        displacementScale: 3000,
+        shininess: 5,
+        shadowSide: THREE.DoubleSide,
+    }),
+    new THREE.MeshToonMaterial({
+        color: 0x92745b,
+        map: floorTexture,
+        bumpMap: heightMapTexture,
+        displacementMap: heightMapTexture,
+        displacementScale: 3000,
+        shadowSide: THREE.DoubleSide,
+    }),
+    new THREE.MeshBasicMaterial({
+        color: 0x92745b,
+        map: floorTexture,
+        shadowSide: THREE.DoubleSide,
+    })
+];
+
+class Terrain extends THREE.Object3D{
+    #terrain
+    constructor(){
+        super();
+        const geometry = new THREE.PlaneGeometry(5000, 5000, 500, 20);
+        this.#terrain = new THREE.Mesh(geometry, terrainMaterials[0]);
+        this.#terrain.rotateX(Math.PI / -2);
+        this.#terrain.geometry.computeVertexNormals();
+        this.add(this.#terrain)
+
+    }
+    changeMaterials(type){
+        this.#terrain.material = terrainMaterials[type];
+    }
+
+}
 
 
 //=========================================================================================================
